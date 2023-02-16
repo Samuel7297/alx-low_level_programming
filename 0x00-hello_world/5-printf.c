1dd4c259ad8825f782acfc4d6e091093e0e34734
Samuel Awuro <sawurosampuo@gmail.com>
2023-02-16 19:31:22 +0000

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 /**
  * main - a program that print a line using the printf function
  * Return: 0 (success)
- */
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
